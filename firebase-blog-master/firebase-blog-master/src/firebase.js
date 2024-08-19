cd92b875ca72dbee46c290be7b0b94c631cde2be
sonakshimathur07 <sonakshimathur117@gmail.com>
2024-08-20 02:12:44 +0530

    first commit

---
+++ firebase-blog-master/firebase-blog-master/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  // firebase
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
